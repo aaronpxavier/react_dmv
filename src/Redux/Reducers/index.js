8837b68477921f9d02cb7aea60c875e03ec39c23
AveryJ13 <59802067+AveryJ13@users.noreply.github.com>
2020-08-19 14:45:04 -0600

    finished vehicle display and made both contacts and vehicles map onto table

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -3,11 +3,13 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import contactReducer from './contactReducer'
+import vehicleReducer from './vehicleReducer'
 
 const allReducers = combineReducers({
   //reducers go here as key value pairs
   authReducer,
-  contactReducer
+  contactReducer,
+  vehicleReducer
 });
 
 export default allReducers;
