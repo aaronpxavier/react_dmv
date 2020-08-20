285e6b00eef195a87b35ea5bdbf2012a19cd0724
agostinii1 <54629275+agostinii1@users.noreply.github.com>
2020-08-20 17:22:21 -0400

    fixed/cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -4,49 +4,14 @@ import { withRouter } from "react-router-dom";
 import NavBar from "./Components/Nav/NavBar";
 import Footer from "./Components/Nav/Footer";
 import configureStore from "./Redux/Store/configureStore";
+import "./App.css";
 
-//const store = configureStore();
-const pageContainer = {
-  // color: "red",
-  // backgroundColor: "green",
-  minHeight: "95vh",
-  position: "relative",
-  opacity: "90%",
-};
-const footerStyle = {
-  // color: "red",
-  position: "absolute",
-  bottom: "0",
-  width: "100%",
-  height: "2.5rem",
-  //marginTop: "100vh",
-  //paddingTop: "100px",
-  // backgroundColor: "red",
-};
-const spacer = {
-  position: "relative",
-  // bottom: "0",
-  height: "10px",
-  // backgroundColor: "red",
-  opacity: "90%",
-};
-const spacerTop = {
-  position: "relative",
-  height: "40px",
-  opacity: "90%",
-  // backgroundColor: "red",
-};
 export class App extends Component {
   render = () => (
-    <div style={pageContainer}>
-      Dmv React Basic
-      <div style={spacerTop} />
+    <div>
       <NavBar />
-      {routes}
-      <div style={spacer} />
-      <div style={footerStyle}>
-        <Footer />
-      </div>
+      <div id="dd">{routes}</div>
+      <Footer />
     </div>
   );
 }
