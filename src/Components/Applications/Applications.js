8f4a75c571d672baac6ed92d96b393c38aafc537
Salvatore Allegra <salallegra@gmail.com>
2020-08-20 09:54:50 -0400

    Add Table imports from material UI

--- src/Components/Applications/Applications.js
+++ src/Components/Applications/Applications.js
@@ -6,6 +6,14 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 
 class Applications extends React.Component {
   async componentDidMount() {
